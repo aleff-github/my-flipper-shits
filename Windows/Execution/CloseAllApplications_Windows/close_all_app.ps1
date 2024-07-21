e38e4fde189438bded205ee2fb350437a4cb2c03
Tom_gxz <75934344+tomgxz@users.noreply.github.com>
2024-07-21 18:17:36 +0100

    Fixed script URL

--- Windows/Execution/CloseAllApplications_Windows/close_all_app.ps1
+++ Windows/Execution/CloseAllApplications_Windows/close_all_app.ps1
@@ -1,5 +1,5 @@
 # Download Python script
-$scriptUrl = "https://raw.githubusercontent.com/aleff-github/my-flipper-shits/main/CloseAllApplications_Windows/script.py"
+$scriptUrl = "https://raw.githubusercontent.com/aleff-github/my-flipper-shits/main/Windows/Execution/CloseAllApplications_Windows/script.py"
 $savePath = "$env:temp\script.py"
 (New-Object System.Net.WebClient).DownloadFile($scriptUrl, $savePath)
 
